2448183155c3e9a9ba62555ee1b4aec0b10c5600
barsalo221 <124167894+barsalo221@users.noreply.github.com>
2024-07-06 20:54:14 +0300

    ConnectInComLine.py
    Connect In Com Line

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-*חיבור למתג דרך SSH*
+# חיבור למתג דרך SSH
 
 import paramiko
 
@@ -10,23 +10,18 @@ def connect_to_switch(ip, username, password, commands):
     for command in commands:
         stdin, stdout, stderr = ssh.exec_command(command)
         print(f"Executing command: {command}")
+        
         print(stdout.read().decode())
         print(stderr.read().decode())
 
     ssh.close()
 
 if __name__ == "__main__":
-    switch_ip = "192.168.1.1"
-    username = "admin"
-    password = "yourpassword"
+    switch_ip = ""
+    username = ""
+    password = ""
     commands = [
-        "show version",
-        "show ip interface brief",
-        "configure terminal",
-        "interface GigabitEthernet0/1",
-        "shutdown",
-        "exit",
-        "write memory"
+        "show version"
     ]
 
     connect_to_switch(switch_ip, username, password, commands)
